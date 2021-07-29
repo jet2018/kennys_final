1948202ce40e6edfd813b13803dabbc6acbd601c
jet2018 <ezrajet9@gmail.com>
2021-07-29 13:26:55 +0300

    added phone smart fields

--- core/settings.py
+++ core/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'phonenumber_field',
     'iprestrict',
     'djmoney',
     'app',  # Enable the inner app
